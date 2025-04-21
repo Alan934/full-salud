bd5c66c395fb4ca4fd2937273518f8de735d7349
Alan934 <sanjurjoalan77@gmail.com>
2025-04-21 18:47:55 -0300

    que no explote 3

--- src/config/database.providers.ts
+++ src/config/database.providers.ts
@@ -11,12 +11,16 @@ export const databaseProviders: TypeOrmModuleOptions = {
   password: envConfig.NODE_ENV === 'development' ? envConfig.DB_PASSWORD_DEV : envConfig.DB_PASSWORD,
   autoLoadEntities: true, // Carga las entidades automáticamente
   synchronize: envConfig.NODE_ENV === 'development', // Sincroniza la base de datos con las entidades (no recomendado para producción)
+  ssl: true, // Obligatorio para Neon.tech y Vercel
   extra: {
     // Configuraciones del pool de conexiones
     max: envConfig.NODE_ENV === 'production' ? 20 : 5, // Máximo de conexiones
     connectionTimeoutMillis: 5000, // Timeout de conexión
     idleTimeoutMillis: 50000, // Cierra conexiones inactivas después de 50 segundos
     allowExitOnIdle: true, // Permite que el proceso de Node.js termine si no hay conexiones activas
+    ssl: {
+      rejectUnauthorized: false
+    }
   },
-  ssl: { rejectUnauthorized: false },
+  //ssl: { rejectUnauthorized: false },
 };
